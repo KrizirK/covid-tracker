db0679d4fb4cc2db1786326306a7f466c0b4ee89
KrizirK <>
2020-10-01 19:59:25 +0200

    Added data download from API and database insert

--- src/main/java/com/learning/covidtracker/runner/CovidTrackerRunner.java
+++ src/main/java/com/learning/covidtracker/runner/CovidTrackerRunner.java
@@ -1,12 +1,43 @@
 package com.learning.covidtracker.runner;
 
-import org.springframework.boot.CommandLineRunner;
+import java.util.Arrays;
+import java.util.List;
 
-public class CovidTrackerRunner implements CommandLineRunner{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import com.learning.covidtracker.dto.CountryDTO;
+import com.learning.covidtracker.dto.mapper.CountryDTOMapper;
+import com.learning.covidtracker.domain.Country;
+import com.learning.covidtracker.domain.repository.CountryRepository;
+
+@Component
+public class CovidTrackerRunner implements CommandLineRunner {
+
+	private final CountryRepository countryRepository;
+
+	private final CountryDTOMapper countryDTOMapper;
+
+	@Autowired
+	public CovidTrackerRunner(CountryRepository countryRepository, CountryDTOMapper countryDTOMapper) {
+		this.countryRepository = countryRepository;
+		this.countryDTOMapper = countryDTOMapper;
+	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+
+		RestTemplate restTemplate = new RestTemplate();
+		CountryDTO[] countries = restTemplate.getForObject("https://api.covid19api.com/total/country/poland",
+				CountryDTO[].class);
+
+		List<CountryDTO> countriesDTO = Arrays.asList(countries);
+
+		List<Country> countryEntities = countryDTOMapper.convertCountriesDTO2Entities(countriesDTO);
+
+		countryRepository.saveAll(countryEntities);
 		
+		System.out.println("Finished!");
 	}
 }
